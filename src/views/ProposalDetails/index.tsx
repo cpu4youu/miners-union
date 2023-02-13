37d668b04acbc86b311977622b9246f026e305fe
unknown <brewerkberly@gmail.com>
2023-02-13 11:15:32 -0800

    Adding proposal details modal

--- src/views/ProposalDetails/index.tsx
+++ src/views/ProposalDetails/index.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import {
   Box,
   Button,
+  Modal,
   IconButton,
   FormControl,
   OutlinedInput,
@@ -15,11 +16,29 @@ import {
 } from "@mui/material";
 
 import SendIcon from "@mui/icons-material/Send";
+import CloseIcon from "@mui/icons-material/Close";
 
 import { makeStyles } from "@mui/styles";
 import PlanetSelect from "./components/PlanetSelect";
 
 import BackButtonIcon from "../../assets/icons/backbutton.png";
+
+const modalStyle = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "300px",
+  bgcolor: "background.paper",
+  background: "#1C1C1C",
+  borderRadius: "20px",
+  border: "1px solid #4D4D4D",
+  outline: "transparent solid 2px",
+  outlineOffset: "2px",
+  boxShadow:
+    "rgba(0, 0, 0, 0.1) 0px 0px 0px 1px,rgba(0, 0, 0, 0.2) 0px 5px 10px,rgba(0, 0, 0, 0.4) 0px 15px 40px",
+  p: 2,
+};
 
 const useStyles = makeStyles({
   contentWrapper: {
@@ -36,10 +55,36 @@ function ProposalDetails() {
   const [selectedPlanet, setSelectedPlanet] = useState("None");
   const classes = useStyles();
   const theme = useTheme();
+  const [openModal, setOpenModal] = useState(false);
+  const [Amount, setAmount] = useState<number>(0);
   let navigate = useNavigate();
   const handleClickMenu = (link: string) => {
     navigate(link);
   };
+
+  const handleAmountChange = (e: any) => {
+    const floatRegExp = new RegExp("([0-9]+([.][0-9]*)?|[.][0-9]+)$");
+    const dotRegExp = new RegExp("^([0-9]+[.][0]*)$");
+    if (e.target.value === "" || floatRegExp.test(e.target.value)) {
+      let filteredValue = e.target.value;
+      if (dotRegExp.test(e.target.value)) {
+        setAmount(filteredValue);
+      } else {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        filteredValue = Math.floor(filteredValue * 1000) / 1000;
+        setAmount(filteredValue);
+      }
+    }
+  };
+
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
+
   const desktop = useMediaQuery(theme.breakpoints.up(1300));
   const mobile = useMediaQuery(theme.breakpoints.down(603));
   return (
@@ -167,6 +212,7 @@ function ProposalDetails() {
                 boxShadow: "inset 0px 0px 36px 1px rgba(54, 0, 206, 0.61)",
                 "&: hover": { opacity: "0.9", background: "#009DF5" },
               }}
+              onClick={() => handleModalOpen()}
             >
               Vote
             </Button>
@@ -192,7 +238,7 @@ function ProposalDetails() {
             }}
           />
           <Box>
-            <Typography fontSize={desktop? "20px" : "18px"} mt={3}>
+            <Typography fontSize={desktop ? "20px" : "18px"} mt={3}>
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
               nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
               erat, sed diam voluptua. At vero eos et accusam et justo duo
@@ -208,7 +254,7 @@ function ProposalDetails() {
               justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
               takimata sanctus est Lorem ipsum dolor sit amet.
             </Typography>
-            <Typography fontSize={desktop? "20px" : "18px"} mt={3}>
+            <Typography fontSize={desktop ? "20px" : "18px"} mt={3}>
               Duis autem veleum iriure dolor in hendrerit in vulputate velit
               esse molestie consequat, vel illum dolore eu feugiat nulla
               facilisis at vero eros et accumsan et iusto odio dignissim qui
@@ -221,6 +267,81 @@ function ProposalDetails() {
             </Typography>
           </Box>
         </Box>
+        <Modal
+          open={openModal}
+          onClose={handleModalClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box color="white" sx={modalStyle}>
+            <Box display="flex" justifyContent="flex-end">
+              <IconButton sx={{ padding: "0" }} onClick={handleModalClose}>
+                <CloseIcon sx={{ color: "white" }} />
+              </IconButton>
+            </Box>
+            <Typography
+              variant="body1"
+              fontSize="16px"
+              mt="24px"
+              textAlign="center"
+              fontFamily="Oxanium Light"
+            >
+              Please choose how many votes you want to spend on this proposal.
+            </Typography>
+            <Typography
+              variant="body1"
+              fontSize="16px"
+              textAlign="center"
+              fontFamily="Oxanium Light"
+              mt="24px"
+              mb="16px"
+            >
+              Higher ranked proposals show the will of the community and are
+              more likely to be accepted by custodians.
+            </Typography>
+            <FormControl sx={{ display: "flex", alignItems: "center", flexGrow: "1" }} variant="outlined">
+              <OutlinedInput
+                id="outlined-adornment-weight"
+                value={Amount}
+                onChange={handleAmountChange}
+                aria-describedby="outlined-weight-helper-text"
+                sx={{
+                  borderRadius: "20px",
+                  color: "white",
+                  width: desktop ? "220px" : "100%",                  
+                  pr: 1,
+                  background: "rgba(121, 121, 121, 0.3)",
+                  border: "1px solid #FFFFFF",
+                  "& .MuiOutlinedInput-input": { padding: "8px 16px" },
+                  "& .MuiOutlinedInput-notchedOutline": { border: "none" },
+                }}
+              />
+            </FormControl>
+            <Button
+              sx={{
+                display: "flex",
+                marginTop: 1,
+                background: "#009DF5",
+                borderRadius: "24px",
+                border: "2px solid #009DF5",
+                textAlign: "center",
+                height: "44px",
+                textTransform: "none",
+                color: "white",
+                m: "12px auto",
+                width: desktop ? "220px" : "100%",
+                lineHeight: "0",
+                fontSize: "20px",
+                fontFamily: "Oxanium Medium",
+                alignItems: "center",
+                boxShadow: "inset 0px 0px 36px 1px rgba(54, 0, 206, 0.61)",
+                "&: hover": { opacity: "0.9", background: "#009DF5" },
+              }}
+            >
+              Vote
+            </Button>
+          </Box>
+        </Modal>
       </Box>
     </>
   );
