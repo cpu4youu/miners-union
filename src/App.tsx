fe11c9a4a258991a3f5aa56a9b5229d2fdba8c4e
unknown <brewerkberly@gmail.com>
2023-02-12 17:10:02 -0800

    Update for Proposal, Create proposal, Proposal detail pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import { GetStarted, Signup, ViewBase, Voting, VotingDetail, Missions, MissionDetails, Information, Contributions } from "./views";
+import {
+  GetStarted,
+  Signup,
+  ViewBase,
+  Voting,
+  VotingDetail,
+  Missions,
+  MissionDetails,
+  Information,
+  Contributions,
+  Proposals,
+  ProposalDetails,
+  CreateProposal,
+} from "./views";
 
 function App() {
   return (
@@ -9,12 +22,78 @@ function App() {
       <Routes>
         <Route path="/" element={<GetStarted />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/voting" element={<ViewBase><Voting /></ViewBase>  } />
-        <Route path="/votingdetail" element={<ViewBase><VotingDetail /></ViewBase>  } />
-        <Route path="/missions" element={<ViewBase><Missions /></ViewBase>  } />
-        <Route path="/missiondetails" element={<ViewBase><MissionDetails /></ViewBase>  } />
-        <Route path="/information" element={<ViewBase><Information /></ViewBase>  } />
-        <Route path="/contributions" element={<ViewBase><Contributions /></ViewBase>  } />
+        <Route
+          path="/voting"
+          element={
+            <ViewBase>
+              <Voting />
+            </ViewBase>
+          }
+        />
+        <Route
+          path="/votingdetail"
+          element={
+            <ViewBase>
+              <VotingDetail />
+            </ViewBase>
+          }
+        />
+        <Route
+          path="/missions"
+          element={
+            <ViewBase>
+              <Missions />
+            </ViewBase>
+          }
+        />
+        <Route
+          path="/missiondetails"
+          element={
+            <ViewBase>
+              <MissionDetails />
+            </ViewBase>
+          }
+        />
+        <Route
+          path="/information"
+          element={
+            <ViewBase>
+              <Information />
+            </ViewBase>
+          }
+        />
+        <Route
+          path="/contributions"
+          element={
+            <ViewBase>
+              <Contributions />
+            </ViewBase>
+          }
+        />
+        <Route
+          path="/proposals"
+          element={
+            <ViewBase>
+              <Proposals />
+            </ViewBase>
+          }
+        />
+        <Route
+          path="/proposaldetails"
+          element={
+            <ViewBase>
+              <ProposalDetails />
+            </ViewBase>
+          }
+        />
+        <Route
+          path="/createproposal"
+          element={
+            <ViewBase>
+              <CreateProposal />
+            </ViewBase>
+          }
+        />
       </Routes>
     </>
   );
